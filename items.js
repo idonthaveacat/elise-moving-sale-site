180b0d51332be2c5e89cabcbe0578b7b7aa16d8a
elisewangxinyi <elisewangxx@gmail.com>
2026-05-02 22:04:05 -0700

    Add TCL TV listing

--- items.js
+++ items.js
@@ -1,9 +1,24 @@
 window.MOVING_SALE_ITEMS = [
   {
+    "id": "8fe153ff-7265-497c-a3fb-fdcf14570d24",
+    "name": "TCL TV",
+    "description": "Almost new. Comes with original box\nCan deliver.",
+    "category": "Furniture",
+    "status": "available",
+    "size": "50\"",
+    "productLink": "",
+    "price": 150,
+    "photos": [
+      "images/tcl-tv-8fe153ff-01.jpg",
+      "images/tcl-tv-8fe153ff-02.jpg"
+    ]
+  },
+  {
     "id": "9142a8ec-7920-4f7d-ae07-682a7855c5ac",
     "name": "Shelf/TV Stand w Storage",
     "description": "Almost new. Comes with instructions.\nPick up preferred. Can take it apart if needed.",
     "category": "Furniture",
+    "status": "available",
     "size": "58\" W x 23.5\" H x 16\" D",
     "productLink": "https://www.ikea.com/us/en/p/kallax-tv-unit-white-stained-oak-effect-50574883/?gad_source=1&gad_campaignid=10699764236&gbraid=0AAAAAD27g7zBwuUIAyWWZwRDr0_AfGZO_&gclid=CjwKCAjwwdbPBhBgEiwAxBRA4TBtFNzehVl125D2MZTfX3yEzKHNrEnhN8K9lAWSzxBCy-WJZIC_cxoCeSoQAvD_BwE",
     "price": 65,
@@ -11,49 +26,48 @@ window.MOVING_SALE_ITEMS = [
       "images/shelf-tv-stand-w-storage-9142a8ec-01.jpg",
       "images/shelf-tv-stand-w-storage-9142a8ec-02.jpg",
       "images/shelf-tv-stand-w-storage-9142a8ec-03.jpg"
-    ],
-    "status": "available"
+    ]
   },
   {
     "id": "1ce94f49-c29e-482e-a189-eba728507202",
     "name": "Yoga Ball",
     "description": "Almost new. Come with air pump.\nCan deliever",
     "category": "Furniture",
+    "status": "available",
     "size": "26\" - 27\" D",
     "productLink": "",
     "price": 10,
     "photos": [
       "images/yoga-ball-1ce94f49-01.jpg",
       "images/yoga-ball-1ce94f49-02.jpg"
-    ],
-    "status": "available"
+    ]
   },
   {
     "id": "9d4105ad-cb3d-4de8-844b-f1e1c16d4543",
     "name": "Full Length Mirror",
     "description": "New. \nCan deliver",
     "category": "Furniture",
+    "status": "available",
     "size": "63\" H x 20.5\" W",
     "productLink": "",
     "price": 40,
     "photos": [
       "images/full-length-mirror-9d4105ad-01.jpg",
       "images/full-length-mirror-9d4105ad-02.jpg"
-    ],
-    "status": "available"
+    ]
   },
   {
     "id": "3260df25-9b81-42d7-9003-5ebc4b7bb98a",
     "name": "Coffee table",
     "description": "Sturdy, almost new condition. \nPick up only",
     "category": "Furniture",
+    "status": "available",
     "size": "44\"W x 21.97\"D x 20\"H",
     "productLink": "https://www.amazon.com/Linon-Home-86151GRY01U-86151Gry01U-RusticCoffee/dp/B00Y5ODVH6?th=1",
     "price": 30,
     "photos": [
       "images/coffee-table-3260df25-01.jpg",
       "images/coffee-table-3260df25-02.jpg"
-    ],
-    "status": "available"
+    ]
   }
 ];
